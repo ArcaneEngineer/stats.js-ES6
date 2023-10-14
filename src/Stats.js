6945b29f1778838d873a144c680d6cd11c6807a3
LAPTOP-0TRG63O7\Nick <nick.wiggill@gmail.com>
2023-10-14 10:09:37 -0300

    (static, nested) ES6 class for Panel as well.

--- src/Stats.js
+++ src/Stats.js
@@ -33,11 +33,11 @@ export default class Stats
 		}, false );
 		this.dom = this.domElement = this.container; // Backwards Compatibility
 		
-		this.fpsPanel = this.addPanel( this.CreatePanel( 'FPS', '#0ff', '#002' ) );
-		this. msPanel = this.addPanel( this.CreatePanel( 'MS' , '#0f0', '#020' ) );
+		this.fpsPanel = this.addPanel( new Stats.Panel( 'FPS', '#0ff', '#002' ) );
+		this. msPanel = this.addPanel( new Stats.Panel( 'MS' , '#0f0', '#020' ) );
 		if ( self.performance && self.performance.memory )
 		{
-		this.memPanel = this.addPanel( this.CreatePanel( 'MB' , '#f08', '#201' ) );
+		this.memPanel = this.addPanel( new Stats.Panel( 'MB' , '#f08', '#201' ) );
 		}
 
 		this.showPanel( 0 );
@@ -98,62 +98,103 @@ export default class Stats
 		this.showPanel(id);
 	}
 	
-	CreatePanel( name, fg, bg )
+	//nested class
+	static Panel = class Panel
 	{
-		var min = Infinity, max = 0, round = Math.round;
-		var PR = round( window.devicePixelRatio || 1 );
-
-		var WIDTH = 80 * PR, HEIGHT = 48 * PR,
-				TEXT_X = 3 * PR, TEXT_Y = 2 * PR,
-				GRAPH_X = 3 * PR, GRAPH_Y = 15 * PR,
-				GRAPH_WIDTH = 74 * PR, GRAPH_HEIGHT = 30 * PR;
-
-		var canvas = document.createElement( 'canvas' );
-		var context = canvas.getContext( '2d' );
+		min = 0
+		max = 0
+		PR = 0
+		WIDTH = 0
+		TEXT_X = 0
+		TEXT_Y = 0
+		GRAPH_X = 0
+		GRAPH_Y = 0
+		GRAPH_WIDTH = 0
+		GRAPH_HEIGHT = 0
 		
-		canvas.width = WIDTH;
-		canvas.height = HEIGHT;
-		canvas.style.cssText = 'width:80px;height:48px';
+		dom = undefined
+		fg = undefined
+		bg = undefined
 		
-		context.font = 'bold ' + ( 9 * PR ) + 'px Helvetica,Arial,sans-serif';
-		context.textBaseline = 'top';
-
-		context.fillStyle = bg;
-		context.fillRect( 0, 0, WIDTH, HEIGHT );
-
-		context.fillStyle = fg;
-		context.fillText( name, TEXT_X, TEXT_Y );
-		context.fillRect( GRAPH_X, GRAPH_Y, GRAPH_WIDTH, GRAPH_HEIGHT );
-
-		context.fillStyle = bg;
-		context.globalAlpha = 0.9;
-		context.fillRect( GRAPH_X, GRAPH_Y, GRAPH_WIDTH, GRAPH_HEIGHT );
-
-		let panel =
+		constructor(name, fg, bg) 
 		{
-			dom: canvas,
+			console.log (`Child: ${name}`);
+			
+			this.name = name;
+			this.fg = fg;
+			this.bg = bg;
+			//numeric config
+			
+			this.min = Infinity;
+			this.max = 0;
+			let PR = this.PR = Math.round( window.devicePixelRatio || 1 );
 
-			update: function ( value, maxValue )
-			{
-				min = Math.min( min, value );
-				max = Math.max( max, value );
+			let WIDTH = this.WIDTH = 80 * PR;
+			let HEIGHT = this.HEIGHT = 48 * PR;
+			let TEXT_X = this.TEXT_X = 3 * PR;
+			let TEXT_Y = this.TEXT_Y = 2 * PR;
+			let GRAPH_X = this.GRAPH_X = 3 * PR;
+			let GRAPH_Y = this.GRAPH_Y = 15 * PR;
+			let GRAPH_WIDTH = this.GRAPH_WIDTH = 74 * PR;
+			let GRAPH_HEIGHT = this.GRAPH_HEIGHT = 30 * PR;
+			
+			//canvas and context
+			let canvas = document.createElement( 'canvas' );
+			let context = canvas.getContext( '2d' );
+			
+			canvas.width = WIDTH;
+			canvas.height = HEIGHT;
+			canvas.style.cssText = 'width:80px;height:48px';
+			
+			context.font = 'bold ' + ( 9 * PR ) + 'px Helvetica,Arial,sans-serif';
+			context.textBaseline = 'top';
 
-				context.fillStyle = bg;
-				context.globalAlpha = 1;
-				context.fillRect( 0, 0, WIDTH, GRAPH_Y );
-				context.fillStyle = fg;
-				context.fillText( round( value ) + ' ' + name + ' (' + round( min ) + '-' + round( max ) + ')', TEXT_X, TEXT_Y );
+			context.fillStyle = bg;
+			context.fillRect( 0, 0, WIDTH, HEIGHT );
 
-				context.drawImage( canvas, GRAPH_X + PR, GRAPH_Y, GRAPH_WIDTH - PR, GRAPH_HEIGHT, GRAPH_X, GRAPH_Y, GRAPH_WIDTH - PR, GRAPH_HEIGHT );
+			context.fillStyle = fg;
+			context.fillText( name, TEXT_X, TEXT_Y );
+			context.fillRect( GRAPH_X, GRAPH_Y, GRAPH_WIDTH, GRAPH_HEIGHT );
 
-				context.fillRect( GRAPH_X + GRAPH_WIDTH - PR, GRAPH_Y, PR, GRAPH_HEIGHT );
+			context.fillStyle = bg;
+			context.globalAlpha = 0.9;
+			context.fillRect( GRAPH_X, GRAPH_Y, GRAPH_WIDTH, GRAPH_HEIGHT );
+			
 
-				context.fillStyle = bg;
-				context.globalAlpha = 0.9;
-				context.fillRect( GRAPH_X + GRAPH_WIDTH - PR, GRAPH_Y, PR, round( ( 1 - ( value / maxValue ) ) * GRAPH_HEIGHT ) );
-			}
-		};
+			this.dom = canvas;
+		}
 		
-		return panel;
+		update ( value, maxValue )
+		{
+			let round = Math.round;
+			let min = this.min = Math.min( this.min, value );
+			let max = this.max = Math.max( this.max, value );
+			let bg = this.bg;
+			let fg = this.fg;
+			let TEXT_X = this.TEXT_X;
+			let TEXT_Y = this.TEXT_Y;
+			let GRAPH_X = this.GRAPH_X;
+			let GRAPH_Y = this.GRAPH_Y;
+			let GRAPH_WIDTH = this.GRAPH_WIDTH;
+			let GRAPH_HEIGHT = this.GRAPH_HEIGHT;
+			let WIDTH = this.WIDTH;
+			let PR = this.PR;
+			let canvas = this.dom;
+			var context = canvas.getContext( '2d' );
+			
+			context.fillStyle = bg;
+			context.globalAlpha = 1;
+			context.fillRect( 0, 0, WIDTH, GRAPH_Y );
+			context.fillStyle = fg;
+			context.fillText( round( value ) + ' ' + this.name + ' (' + round( min ) + '-' + round( max ) + ')', TEXT_X, TEXT_Y );
+
+			context.drawImage( canvas, GRAPH_X + PR, GRAPH_Y, GRAPH_WIDTH - PR, GRAPH_HEIGHT, GRAPH_X, GRAPH_Y, GRAPH_WIDTH - PR, GRAPH_HEIGHT );
+
+			context.fillRect( GRAPH_X + GRAPH_WIDTH - PR, GRAPH_Y, PR, GRAPH_HEIGHT );
+
+			context.fillStyle = bg;
+			context.globalAlpha = 0.9;
+			context.fillRect( GRAPH_X + GRAPH_WIDTH - PR, GRAPH_Y, PR, round( ( 1 - ( value / maxValue ) ) * GRAPH_HEIGHT ) );
+		}
 	}
 }
